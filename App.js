4c924e4bd3aab180841505ade431375f2e74fce1
Charlie Harrington <charles.harrington@gmail.com>
2022-09-17 21:18:34 -0700

    setup expo

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const NIBBLES = [
+  '0000',
+  '0001',
+  '0010',
+  '0011',
+  '0100',
+  '0101',
+  '0110',
+  '0111',
+  '1000',
+  '1001',
+  '1010',
+  '1011',
+  '1100',
+  '1101',
+  '1110',
+  '1100',
+];
+
 export default function App() {
+  const random_idx = Math.floor(Math.random() * NIBBLES.length);
+  const random_nibble = NIBBLES[random_idx];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Nibble</Text>
+      <Text>{random_nibble}</Text>
+      <Text>{random_idx}</Text>
       <StatusBar style="auto" />
     </View>
   );
